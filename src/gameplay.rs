28bc1e4ae58418746dc7237fcfd2857a02d7b1fb
90degs2infty <90degs2infty@posteo.org>
2023-10-24 21:11:16 +0200

    Begin implementing game's state machine

--- src/gameplay.rs
+++ src/gameplay.rs
@@ -3,7 +3,49 @@ use crate::geometry::{
     tile::{BasicTile, DisplacedTile, RotatedTile},
 };
 
-struct Game<const M: usize, const N: usize> {
-    tile: Option<DisplacedTile<RotatedTile<BasicTile>>>,
+use either::Either;
+
+pub struct TileNeeded {}
+pub struct TileFloating {}
+pub struct Over {}
+
+// TODO trait sealing
+
+pub trait State {
+    type Tile;
+}
+
+impl State for TileNeeded {
+    type Tile = ();
+}
+
+impl State for TileFloating {
+    type Tile = DisplacedTile<RotatedTile<BasicTile>>;
+}
+
+impl State for Over {
+    type Tile = ();
+}
+
+pub struct Game<T, const M: usize, const N: usize>
+where
+    T: State,
+{
+    tile: T::Tile,
     board: Board<M, N>,
+}
+
+impl<const M: usize, const N: usize> Game<TileNeeded, M, N> {
+    pub fn new() -> Self {
+        Self {
+            tile: (),
+            board: [[false; N]; M],
+        }
+    }
+
+    pub fn place_tile(
+        tile: <TileFloating as State>::Tile,
+    ) -> Either<Game<TileFloating, M, N>, Game<Over, M, N>> {
+        todo!("To be implemented")
+    }
 }
