3b309d833121a51d668c81c663c7e79c2dae3206
90degs2infty <90degs2infty@posteo.org>
2023-10-24 20:55:08 +0200

    Replace loop by iteration

--- src/rendering.rs
+++ src/rendering.rs
@@ -9,10 +9,10 @@ where
     T: Set,
 {
     fn render<const M: usize, const N: usize>(&self, board: &mut Board<M, N>) {
-        for row in 0..M {
-            for col in 0..N {
+        for (r, row) in board.iter_mut().enumerate() {
+            for (c, square) in row.iter_mut().enumerate() {
                 // x corresponds to columns and y corresponds to rows!
-                board[row][col] = self.contains(col as i32, row as i32);
+                *square = self.contains(c as i32, r as i32);
             }
         }
     }
