9bf17685ce63e4a22e12ab8f35e855e87b415031
90degs2infty <90degs2infty@posteo.org>
2023-11-12 15:44:25 +0100

    chore: implement `set_element`

--- src/geometry/grid.rs
+++ src/geometry/grid.rs
@@ -1,3 +1,5 @@
+use super::board::{BOARD_COLS, BOARD_ROWS};
+
 #[derive(Debug)]
 pub enum GridError {
     InvalidIndex,
@@ -11,12 +13,32 @@ impl Grid {
         Self { 0: grid }
     }
 
+    // elements are encoded row major
+    const fn element_to_bit_idx(row: usize, col: usize) -> Option<usize> {
+        if row >= BOARD_ROWS || col >= BOARD_COLS {
+            None
+        } else {
+            Some(row * BOARD_COLS + col)
+        }
+    }
+
+    const fn element_bit(row: usize, col: usize) -> Option<u32> {
+        // as of now, you cannot call `.map()` on an `Option` from
+        // a const function, so we have to rebuild `map` locally
+        match Self::element_to_bit_idx(row, col) {
+            None => None,
+            Some(idx) => Some(1 << idx),
+        }
+    }
+
     pub fn overlaps(&self, other: &Grid) -> bool {
         (self.0 & other.0) != 0
     }
 
     pub fn set_element(self, row: usize, col: usize) -> Result<Self, GridError> {
-        todo!()
+        Self::element_bit(row, col)
+            .map(|bit| Self::new(self.0 | bit))
+            .ok_or(GridError::InvalidIndex)
     }
 
     pub fn is_element_set(&self, row: usize, col: usize) -> Result<bool, GridError> {
