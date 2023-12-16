88ece49ab168687bfc2b2f1768a192b47be42d4c
90degs2infty <90degs2infty@posteo.org>
2023-12-16 10:52:04 +0100

    Temporarily ignore missing docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 #![no_std]
 #![allow(clippy::module_name_repetitions)]
+// temporarily ignore missing docs
+#![allow(
+    missing_docs,
+    rustdoc::missing_crate_level_docs,
+    clippy::missing_errors_doc,
+    clippy::missing_panics_doc
+)]
 
 pub mod gameplay;
 pub mod geometry;
